7925339c75c41aeb2ba498be98cfd44274cd48e5
gamer-does-micro-bit <135429281+gamer-does-micro-bit@users.noreply.github.com>
2023-06-03 13:23:00 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+music.playMelody("B A G A G F A C5 ", 120)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.forever(function () {
+	
+})
